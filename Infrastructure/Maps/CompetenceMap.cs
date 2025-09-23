3f9160c8a3644f0f8990110edbfc72e33ee69eba
HUGO RICARDO ALMEIDA GUIMARÃES <8220337@estg.ipp.pt>
2025-09-23 12:49:20 +0100

    #add: Competence Map

---
+++ Infrastructure/Maps/CompetenceMap.cs
@@ -0,0 +1,16 @@
+﻿
+using CorujinhaAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Maps
+{
+    internal class CompetenceMap : IEntityTypeConfiguration<Competence>
+    {
+        public void Configure(EntityTypeBuilder<Competence> builder)
+        {
+            builder.ToTable(nameof(Competence));
+            builder.HasKey(e => e.Id);
+        }
+    }
+}
