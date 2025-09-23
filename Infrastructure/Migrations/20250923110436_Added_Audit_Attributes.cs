1bed521fbf1d993c2ebfc399489c338da2f8775a
HUGO RICARDO ALMEIDA GUIMARÃES <8220337@estg.ipp.pt>
2025-09-23 12:47:21 +0100

    #add: Audit atributes migration

---
+++ Infrastructure/Migrations/20250923110436_Added_Audit_Attributes.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Audit_Attributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_EmployeeCategory_EmployeeCategoryId",
+                table: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeCategory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_EmployeeCategoryId",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeCategoryId",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Employee",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Employee",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Employee",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Competence",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Competence", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trainer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Training",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Training", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeCompetence",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeIdId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompetenceIdId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeCompetence", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeCompetence_Competence_CompetenceIdId",
+                        column: x => x.CompetenceIdId,
+                        principalTable: "Competence",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeCompetence_Employee_EmployeeIdId",
+                        column: x => x.EmployeeIdId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrainerCompetence",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TrainerIdId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompetenceIdId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainerCompetence", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TrainerCompetence_Competence_CompetenceIdId",
+                        column: x => x.CompetenceIdId,
+                        principalTable: "Competence",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TrainerCompetence_Trainer_TrainerIdId",
+                        column: x => x.TrainerIdId,
+                        principalTable: "Trainer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrainerTraining",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TrainerIdId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TrainingIdId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainerTraining", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TrainerTraining_Trainer_TrainerIdId",
+                        column: x => x.TrainerIdId,
+                        principalTable: "Trainer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TrainerTraining_Training_TrainingIdId",
+                        column: x => x.TrainingIdId,
+                        principalTable: "Training",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeCompetence_CompetenceIdId",
+                table: "EmployeeCompetence",
+                column: "CompetenceIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeCompetence_EmployeeIdId",
+                table: "EmployeeCompetence",
+                column: "EmployeeIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainerCompetence_CompetenceIdId",
+                table: "TrainerCompetence",
+                column: "CompetenceIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainerCompetence_TrainerIdId",
+                table: "TrainerCompetence",
+                column: "TrainerIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainerTraining_TrainerIdId",
+                table: "TrainerTraining",
+                column: "TrainerIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainerTraining_TrainingIdId",
+                table: "TrainerTraining",
+                column: "TrainingIdId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeCompetence");
+
+            migrationBuilder.DropTable(
+                name: "TrainerCompetence");
+
+            migrationBuilder.DropTable(
+                name: "TrainerTraining");
+
+            migrationBuilder.DropTable(
+                name: "Competence");
+
+            migrationBuilder.DropTable(
+                name: "Trainer");
+
+            migrationBuilder.DropTable(
+                name: "Training");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "EmployeeCategoryId",
+                table: "Employee",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeCategory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_EmployeeCategoryId",
+                table: "Employee",
+                column: "EmployeeCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_EmployeeCategory_EmployeeCategoryId",
+                table: "Employee",
+                column: "EmployeeCategoryId",
+                principalTable: "EmployeeCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
